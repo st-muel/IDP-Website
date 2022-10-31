021f91ef1328917d44b798d5ccb7fb83def4fef2
st-muel <39916027+st-muel@users.noreply.github.com>
2022-10-31 18:59:30 +0800

    Update datetime-select.js

--- js/datetime-select.js
+++ js/datetime-select.js
@@ -1,5 +1,7 @@
 $().ready(function() {
-	$('#date_picker_from').attr('min', new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
-	$('#date_picker_to').attr('min', new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
-	$('#date_picker_from').attr('value', new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
+	var date = new Date("October 30, 2022 5:00:00");
+	$('#date_picker_from').attr('min', date.toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
+	$('#date_picker_to').attr('min', date.toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
+	$('#date_picker_from').attr('value', date.toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
+	$('#date_picker_to').attr('value', date.toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
 })
