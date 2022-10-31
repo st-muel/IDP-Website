4849658b8e3503108f9466e86282f5a33ac7969d
st-muel <39916027+st-muel@users.noreply.github.com>
2022-10-31 18:52:30 +0800

    Changed stuff

--- js/datetime-select.js
+++ js/datetime-select.js
@@ -3,8 +3,3 @@ $().ready(function() {
 	$('#date_picker_to').attr('min', new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
 	$('#date_picker_from').attr('value', new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":")));
 })
-
-$('.search-btn').on('click', function(e) {
-	e.preventDefault();
-	document.location.href = '/IDP-Website/success.html'
-})
